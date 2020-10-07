ef5b0fbbb602f7cf612d02831dfc99ec4fc9d50c
SakilMondal <mondalsakil@gmail.com>
2020-10-07 12:45:35 +0530

    sdm660-common: Build QTI thermal 2.0 hal
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- common.mk
+++ common.mk
@@ -387,7 +387,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1-service.sdm660
+    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_PACKAGES += \
