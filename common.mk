5c802e96d312e28c4b3a0984c475946caa2074e0
trpsl <trpsl755@gmail.com>
2021-01-10 12:36:36 +0530

    sdm660-common :adapt new MotoActions

--- common.mk
+++ common.mk
@@ -229,9 +229,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sdm660
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Media
 PRODUCT_COPY_FILES += \
